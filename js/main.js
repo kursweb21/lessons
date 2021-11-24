4a6c911d20f9f8372d7b04bc8882383f63b4afeb
Kovylov Sergey <kursweb2021@gmail.com>
2021-11-24 22:20:40 +0300

    добавили функционал в модальное окно

--- js/main.js
+++ js/main.js
@@ -2,14 +2,76 @@
 const btnCart = document.getElementById('cart')
 const modal = document.querySelector('.modal')
 const btnClose = document.querySelector('.btn-close')
-const rows = modal.querySelectorAll('.row');
 const total = modal.querySelector('.modal-sum');
+const mainIndex = document.getElementById('index')
+const mainRest = document.getElementById('restourant')
+const cardsBlockIndex = mainIndex.querySelector('.cards')
+const cardsIndex = cardsBlockIndex.querySelectorAll('.card')
+const logos = document.querySelectorAll('.logo-link')
+const cardsBlockRest = mainRest.querySelector('.cards')
+const cardsRest = cardsBlockRest.querySelectorAll('.card')
+
+let rows = modal.querySelectorAll('.row');
+let nameArr = []
 
 // объявление функций
+const getProductModal = (nameProduct, priceProduct, oldPrice, index = 0) => {
+    const rowsBlock = modal.querySelector('.modal-main')
+    rows = rowsBlock.querySelectorAll('.row')
+    const newRow = rows[0].cloneNode(true)
+    const nameProductBlock = newRow.querySelector('.product-name');
+    const priceProductBlock = newRow.querySelector('.price');
+    newRow.classList.remove('not--active')
+    nameProductBlock.textContent = nameProduct
+    priceProductBlock.textContent = oldPrice
+    rowsBlock.append(newRow)
+    if (nameArr.includes(nameProduct)) {
+        newRow.remove()
+    }
+
+    nameArr.push(nameProduct)
+    
+
+    rows = rowsBlock.querySelectorAll('.row')
+    rows.forEach(row => {
+        let nameProductBlock = row.querySelector('.product-name');
+        let priceProductBlock = newRow.querySelector('.price');
+        let priceBlock = row.querySelector('.price');
+        let price = +priceBlock.textContent;
+        let countBlock = row.querySelector('.count');
+        let count = countBlock.textContent;
+        const btnMinus = row.querySelector('.minus');
+        const btnPlus = row.querySelector('.plus');
+        console.log();
+        if (nameProductBlock.textContent == nameProduct) {
+            count++
+            countBlock.textContent = count
+            getNewPrice(count, price, priceBlock)
+        }
+    
+    
+    
+        btnMinus.addEventListener('click', () => {
+            if (count > 0) {
+                count--
+                countBlock.textContent = count
+                getNewPrice(count, price, priceBlock)
+            } else if (count == 0) {
+                getNewPrice(count, oldPrice, priceBlock)
+            }
+        })
+        btnPlus.addEventListener('click', () => {
+            count++
+            countBlock.textContent = count
+            getNewPrice(count, price, priceBlock)
+        })
+    })
+
+
+}
 const modalOpen = () => {
     modal.classList.toggle('modal--close')   
 }
-
 const getFullPrice = () => {
     let fullPrice = 0;
     rows.forEach(row => {
@@ -19,45 +81,56 @@ const getFullPrice = () => {
     })
     total.textContent = fullPrice
 }
-
+const getToMain = () => {
+    mainRest.classList.toggle('not--active')
+    mainIndex.classList.toggle('not--active')
+}
+const getNewPrice = (count, oldPrice, priceBlock) => {
+    let newPrice = 0;
+    newPrice = count * oldPrice
+    priceBlock.textContent = newPrice
+    getFullPrice()
+}
 // вызов функций
-getFullPrice()
-
 btnCart.addEventListener('click', modalOpen);
 btnClose.addEventListener('click', modalOpen);
-
 modal.addEventListener('click', (event) => {
     if (event.target == modal) {
         modalOpen()
     }
 })
-rows.forEach(row => {
-    let newPrice = 0;
-    let priceBlock = row.querySelector('.price')
-    let price = +priceBlock.textContent
-    let countBlock = row.querySelector('.count')
-    let count = countBlock.textContent
-    const btnMinus = row.querySelector('.minus')
-    const btnPlus = row.querySelector('.plus')
 
+cardsRest.forEach((card, index) => {
+    let nameProductBlock = card.querySelector('.cart-title')
+    let priceProductBlock = card.querySelector('.product-price')
+    let nameProduct = nameProductBlock.textContent
+    let priceProduct = +priceProductBlock.textContent.slice(0, 3)
+    let oldPrice = priceProduct
+    const btnProduct = card.querySelector('button')
 
-    const getNewPrice = (count, price) => {
-        newPrice = count * price
-        priceBlock.textContent = newPrice
-        getFullPrice()
-    }
-
-    btnMinus.addEventListener('click', () => {
-        if (count > 0) {
-            count--
-            countBlock.textContent = count
-            getNewPrice(count, price)
-        }
+    btnProduct.addEventListener('click', () => {
+        getProductModal(nameProduct, priceProduct, oldPrice, index)
     })
-    btnPlus.addEventListener('click', () => {
-        count++
-        countBlock.textContent = count
-        getNewPrice(count, price)
+    
+})
+cardsIndex.forEach(card => {
+    card.addEventListener('click', () => {
+        getToMain()
     })
 })
+logos.forEach(logo => {
+    logo.addEventListener('click', (event) => {
+        event.preventDefault()
+        if (mainIndex.classList.contains('not--active')) {
+            getToMain()
+        }
+    })
+})
+getFullPrice()
 // логеры
+
+
+
+
+
+
